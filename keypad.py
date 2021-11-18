4b34d7f04b0a11b4ec6b618c554b4ec14b88c249
Alex <dextor@gmail.com>
2021-11-18 22:47:12 +0100

    Say next track name WIP

--- keypad.py
+++ keypad.py
@@ -5,33 +5,13 @@
 #
 from pad4pi import rpi_gpio
 from mpd import MPDClient
+from multiprocessing import Process
+from threading import Thread
 import os
 import time
 
 client = MPDClient()
 host = 'localhost'
-
-def connect():
-    connected = False
-    status = None
-    try:
-        client.status()
-        connected = True
-    except:
-        print('Not connected')
-
-
-    while connected == False:
-        try:
-            print('Connecting', host)
-            client.timeout = 20
-            client.idletimeout = None
-            client.connect(host, 6600)
-            connected = True
-        except:
-            time.sleep(1)
-
-connect()
 
 KEYPAD = [
     [1, 2],
@@ -41,6 +21,7 @@ KEYPAD = [
 # BCM Pins
 ROW_PINS = [26, 20]
 COL_PINS = [19, 16]
+
 
 def handleKey(key):
     connect()
@@ -66,19 +47,54 @@ def handleKey(key):
 
 def say(message):
     print(message)
-    client.volume(-50)
+    client.volume(-10)
     os.system('echo "{0}" | festival --tts'.format(message))
-    client.volume(50)
+    client.volume(10)
 
 
 def execute(command):
     out = check_output(command)
     return out.decode('utf-8').strip()
 
-status = client.status()
+
+def connect():
+    connected = False
+    status = None
+    try:
+        client.status()
+        connected = True
+    except:
+        print('Not connected')
+
+    while connected == False:
+        try:
+            print('Connecting', host)
+            client.timeout = None
+            client.idletimeout = None
+            client.connect(host, 6600)
+            connected = True
+        except:
+            time.sleep(1)
+
+
+def checkchanges():
+    idleclient = MPDClient()
+    idleclient.connect(host, 6600)
+    while True:
+        result = idleclient.idle('player')
+        status = idleclient.status()
+        if(status['state'] == 'play'):
+            song = client.currentsong()
+            say(song['artist'])
+
+
+connect()
 client.stop()
-say('MusicBox is Ready. {0} tracks in the playlist.'.format(status['playlistlength']))
+say('MusicBox is Ready.')
 client.play()
+#t1 = Thread(target=checkchanges)
+#t1.setDaemon(True)
+#t1.start()
 
 try:
     factory = rpi_gpio.KeypadFactory()
